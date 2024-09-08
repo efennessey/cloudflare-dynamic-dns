e2ab27dfce6d2845f7bb5458a447b39857fc1a01
Ed Fennessey <fennessey.edward@gmail.com>
2024-09-08 19:46:31 -0400

    added response validation

--- cloudflare-ddns.py
+++ cloudflare-ddns.py
@@ -6,28 +6,44 @@ import public_ip
 def getRecords(zone):
     endpoint = "https://api.cloudflare.com/client/v4/zones/" + zone + "/dns_records/"
     response = requests.get(endpoint, headers=headers)
-    #TODO: validate response
+    validateResponse(response, zone, "List DNS Records")
     return response.json()["result"]
 
 def updateRecord(zone, record):
     endpoint = "https://api.cloudflare.com/client/v4/zones/" + zone + "/dns_records/" + record["id"]
     body = json.dumps({"content": ip})
     response = requests.patch(endpoint, headers=headers, data=body)
-    #TODO: validate response
-    #TODO: return records that were actually updated
+    if validateResponse(response, zone, "Update DNS Record"):
+        return response.json()["result"]
+    else:
+        return False
+
+def validateResponse(response, zone, requestType):
+    if response.status_code != 200:
+        print("Bad request - " + requestType + " for zone ID " + zone)
+        print(response.json()["errors"])
+        print(response.raise_for_status())
+        return False
+    elif not response.json()["success"]:
+        print("Errors - " + requestType + " for zone ID " + zone)
+        print(response.json()["errors"])
+        raise ValueError(requestType + " request was not successful")
+        return False
+    else:
+        return True
 
 
 
 config = json.load(open(sys.argv[1]))
 headers = {"Authorization": "Bearer " + config["bearerToken"]}
 ip = public_ip.get()
-recordNames =[]
+recordNames = []
 
 for domain in config["domains"]:
     for record in getRecords(domain["zoneId"]):
-        updateRecord(domain["zoneId"], record)
-        #TODO use recordNames returned from updateRecord
-        recordNames.append(record["name"])
+        newRecord = updateRecord(domain["zoneId"], record)
+        if newRecord:
+            recordNames.append(newRecord["name"])
 
 print("Updated the following records to " + ip + ":")
 for name in recordNames:
